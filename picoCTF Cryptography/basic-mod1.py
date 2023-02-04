5e10d21b73e51605007595dba6fc54020104b3d3
Vaibhav-Sandhir <vaibhavsandhir1@gmail.com>
2023-02-04 05:39:12 +0000

    BasicMod 1

---
+++ picoCTF Cryptography/basic-mod1.py
@@ -0,0 +1,19 @@
+list_cipher = [91,322,57,124,40,406,272,147,239,285,353,272,77,110,296,262,299,323,255,337,150,102]
+list_decipher = []
+plain_text = ""
+
+for c in list_cipher:
+    num = (c % 37)
+    print(num)
+    if(num == 36):
+        plain_text += "_"
+    elif(num >= 26 and num <= 35):
+        num = num - 26
+        plain_text += str(num)
+    elif(num >= 0 and num <= 25):
+        char = chr(num + 65)
+        plain_text += char
+
+
+print("picoCTF{" + plain_text + "}")    
+                       
